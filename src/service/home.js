02c102bfee424ba0f60626cd2e27d8d23e440e53
gaoying <1335029903@qq.com>
2019-01-21 15:03:12 +0800

    update

--- src/service/home.js
+++ src/service/home.js
@@ -2,9 +2,9 @@
  * Created by gaoying on 2017/10/31.
  */
 
-import { get } from "../tools/async-tool";
+import { get } from '../tools/async-tool'
 
-export function getData(cb) {
-  let url = 'http://10.132.20.14:8083/mockjsdata/62/getData';
-  return get(url);
+export function getData (cb) {
+  let url = ''
+  return get(url)
 }
